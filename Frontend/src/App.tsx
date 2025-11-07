00df9285d64098707b3b4429a3f1cd13356dd1f2
2020331002_Tanvir <95771942+rayhanulamint2@users.noreply.github.com>
2025-11-07 23:03:18 +0600

    primary home page created.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,32 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import "./App.css";
+
+
+// import "./App.css";
+// import LandingPage from "./Components/LandingPage";
+
+// function App() {
+//   return <LandingPage />;
+// }
+
+// export default App;
+
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./Components/LandingPage";
+import Home from "./Components/Home";
+import "./App.css";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
